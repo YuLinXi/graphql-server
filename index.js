69f01f19a0bf49ee7befa1f7b0eddea0279761fc
YuLinXi <15708444077@163.com>
2021-08-29 18:38:56 +0800

    :sparkles: graphql-apollo-express

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const { ApolloServer, gql } = require("apollo-server");
+const express = require("express");
+const { ApolloServer, gql } = require("apollo-server-express");
 
 const typeDefs = gql`
   type Book {
@@ -24,8 +25,21 @@ const resolvers = {
   },
 };
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const apolloServer = new ApolloServer({ typeDefs, resolvers });
+apolloServer.start().then(() => {
+  const app = express();
 
-server.listen().then(({ url }) => {
-  console.log(`Server ready at ${url}`);
+  apolloServer.applyMiddleware({ app });
+
+  app.use((req, res) => {
+    res.status(200);
+    res.send("Hello!");
+    res.end();
+  });
+
+  app.listen({ port: 8899 }, () => {
+    console.log(
+      `Server ready at http://localhost:8899${apolloServer.graphqlPath}`
+    );
+  });
 });
